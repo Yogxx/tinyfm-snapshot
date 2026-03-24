ac00393c68c77b6c925b5a30eb537ce0fd8225d9
anu <163709469+Yogxx@users.noreply.github.com>
2026-03-24 12:38:00 +0700

    Update tinyfm.js

--- luci-app-tinyfm/root/www/luci-static/resources/view/services/tinyfm.js
+++ luci-app-tinyfm/root/www/luci-static/resources/view/services/tinyfm.js
@@ -4,7 +4,7 @@
 return view.extend({
     render: function() {
         var frame = E('iframe', {
-            src: window.location.protocol + '//' + window.location.host + '/tinyfm/TinyFM.php',
+            src: window.location.protocol + '//' + window.location.host + '/tinyfm/index.php',
             style: 'width: 100%; min-height: 100vh; border: none; border-radius: 4px;'
         });
 
